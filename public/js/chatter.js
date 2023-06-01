937fb90c90059438f65129230be1e64f3330c106
mark-gutierrez <mark.gtrez@gmail.com>
2023-06-01 22:43:31 +1000

    add new chatters

--- public/js/chatter.js
+++ public/js/chatter.js
@@ -6,61 +6,107 @@ const socket = new WebSocket(
 
 function Online() {
     const [username, setUsername] = useState("")
-    const [userConvos, setuserConvos] = useState([])
+    const [userConvos, setUserConvos] = useState({})
+    const [convoData, setConvoData] = useState({})
     const [convoWith, setConvoWith] = useState({
-        name: "",
+        username: "",
         conversation_uid: "",
     })
     const [messages, setMessages] = useState([])
     const [text, setText] = useState("")
+    const [searchedUsers, setSearchedUsers] = useState([])
 
-    socket.onopen = (event) => {
+    socket.onopen = function (event) {
         socket.send(JSON.stringify({ type: "init" }))
     }
 
-    socket.onmessage = (message) => {
+    socket.onmessage = function (message) {
         const data = JSON.parse(message.data)
 
         if (data.type === "init") {
             setUsername(data.username)
-            setuserConvos(data.userConvos)
-        }
+            const { userConvos, userMessages } = data
+            let obj = {}
+            let obj1 = {}
 
-        if (data.type === "getMessages") {
-            setMessages(data.messages)
-            console.log(data.messages)
+            for (let { conversation_uid, username } of userConvos) {
+                obj[conversation_uid] = obj[conversation_uid] || username
+                obj1[conversation_uid] = obj1[conversation_uid] || []
+            }
+
+            for (let message of userMessages) {
+                obj1[message.conversation_uid].push(message)
+            }
+
+            setUserConvos(obj)
+            setConvoData(obj1)
         }
 
         if (data.type === "sendMessage") {
-            console.log(data)
-            updateMessages(data)
+            updateMessages(data.message)
+        }
+
+        if (data.type === "searchUser") {
+            setSearchedUsers(data.users)
+        }
+
+        if (data.type === "addNewChatter") {
+            updateConvos(data.newConvo[0])
         }
     }
 
+    function updateConvos(newConvo) {
+        const { username, conversation_uid } = newConvo
+        const newConvoData = JSON.parse(JSON.stringify(convoData))
+        newConvoData[conversation_uid] = []
+        setConvoData(newConvoData)
+        const newUserConvo = JSON.parse(JSON.stringify(userConvos))
+        newUserConvo[conversation_uid] = username
+        setUserConvos(newUserConvo)
+        socket.send(
+            JSON.stringify({
+                type: "addNewConvo",
+                data: conversation_uid,
+            })
+        )
+    }
+
     function updateMessages(data) {
-        if (convoWith.conversation_uid === data.message.conversation_uid) {
-            setMessages([...messages, data.message])
+        const { conversation_uid } = data
+        const conversations = JSON.parse(JSON.stringify(convoData))
+        conversations[conversation_uid].push(data)
+        setConvoData(conversations)
+        if (conversation_uid === convoWith.conversation_uid) {
+            setMessages([...messages, data])
         }
-        setText("")
     }
 
     function handleClickConversation(e) {
         e.preventDefault()
 
-        setConvoWith({
-            name: e.target.innerText,
-            conversation_uid: e.target.value,
-        })
-        socket.send(
-            JSON.stringify({
-                type: "getMessages",
-                data: e.target.value,
+        const { innerText: username, value: conversation_uid } = e.target
+        if (
+            convoWith.username === username &&
+            convoWith.conversation_uid === conversation_uid
+        ) {
+            setConvoWith({
+                username: "",
+                conversation_uid: "",
             })
-        )
+            setMessages([])
+        } else {
+            setConvoWith({
+                username,
+                conversation_uid,
+            })
+            setMessages(convoData[conversation_uid])
+        }
     }
 
     function handleSubmitMessage(e) {
         e.preventDefault()
+
+        if (text.length === 0) return
 
         socket.send(
             JSON.stringify({
@@ -71,28 +117,37 @@ function Online() {
                 },
             })
         )
+        setText("")
     }
 
     return (
         <div>
             <h1>Hi {username}</h1>
-            <ul>
-                {userConvos.length > 0 &&
-                    userConvos.map(
-                        ({ conversation_uid, user_uid, username }) => (
-                            <li key={user_uid}>
-                                <button
-                                    onClick={handleClickConversation}
-                                    value={conversation_uid}
-                                >
-                                    {username}
-                                </button>
-                            </li>
-                        )
-                    )}
-            </ul>
+            <Navbar />
+            <SearchUser
+                searchedUsers={searchedUsers}
+                setSearchedUsers={setSearchedUsers}
+            />
             <div>
-                {convoWith.name !== "" && <h2>{convoWith.name}</h2>}
+                <h2>Chatters</h2>
+                <ul>
+                    {Object.keys(userConvos).length > 0 &&
+                        Object.entries(userConvos).map(
+                            ([conversation_uid, username]) => (
+                                <li key={conversation_uid}>
+                                    <button
+                                        onClick={handleClickConversation}
+                                        value={conversation_uid}
+                                    >
+                                        {username}
+                                    </button>
+                                </li>
+                            )
+                        )}
+                </ul>
+            </div>
+            <div>
+                {convoWith.username !== "" && <h2>{convoWith.username}</h2>}
                 <ul>
                     {messages.length > 0 &&
                         messages.map(({ message_uid, text, username }) => (
@@ -101,7 +156,7 @@ function Online() {
                             </li>
                         ))}
                 </ul>
-                {convoWith.name !== "" && (
+                {convoWith.username !== "" && (
                     <form onSubmit={handleSubmitMessage}>
                         <input
                             type="text"
@@ -113,6 +168,106 @@ function Online() {
                 )}
             </div>
         </div>
+    )
+}
+
+function SearchUser({ searchedUsers, setSearchedUsers }) {
+    const [searchUsersButton, setSearchUsersButton] = useState(false)
+    const [searchUsers, setSearchUsers] = useState("")
+
+    function handleClickNewChatter(e) {
+        e.preventDefault()
+        setSearchUsersButton(!searchUsersButton)
+        setSearchUsers("")
+        setSearchedUsers([])
+    }
+
+    function handleSubmitSearchUsers(e) {
+        e.preventDefault()
+
+        socket.send(
+            JSON.stringify({
+                type: "searchUser",
+                data: searchUsers,
+            })
+        )
+    }
+
+    function addNewChatter(e) {
+        e.preventDefault()
+
+        socket.send(
+            JSON.stringify({
+                type: "addNewChatter",
+                data: { user_uid: e.target.value },
+            })
+        )
+
+        setSearchUsersButton(!searchUsersButton)
+        setSearchUsers("")
+        setSearchedUsers([])
+    }
+
+    return (
+        <div>
+            <button type="button" onClick={handleClickNewChatter}>
+                {searchUsersButton ? "-" : "+"} New Chatter
+            </button>
+            {searchUsersButton && (
+                <form onSubmit={handleSubmitSearchUsers}>
+                    <input
+                        type="text"
+                        name="text"
+                        value={searchUsers}
+                        onChange={(e) => setSearchUsers(e.target.value)}
+                    ></input>
+                </form>
+            )}
+            {searchUsersButton && (
+                <ul>
+                    {searchedUsers.length > 0 &&
+                        searchedUsers.map(({ user_uid, username }) => {
+                            return (
+                                <li key={user_uid}>
+                                    <button
+                                        onClick={addNewChatter}
+                                        value={user_uid}
+                                    >
+                                        + {username}
+                                    </button>
+                                </li>
+                            )
+                        })}
+                </ul>
+            )}
+        </div>
+    )
+}
+
+function Navbar() {
+    async function logout(e) {
+        e.preventDefault()
+        const { loggedOut } = await fetch("/logout", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({}),
+        }).then((data) => data.json())
+        if (loggedOut === true) {
+            window.location.replace("/")
+        }
+    }
+    return (
+        <nav>
+            <ol>
+                <li>
+                    <button type="button" onClick={logout}>
+                        Logout
+                    </button>
+                </li>
+            </ol>
+        </nav>
     )
 }
 
--- web/online.js
+++ web/online.js
@@ -1,8 +1,19 @@
+const { randomUUID } = require("node:crypto")
+
 module.exports = function (fastify, opts, done) {
+    fastify.post("/logout", async function (request, reply) {
+        if (request.session.user) {
+            request.session.destroy()
+            return reply.send({ loggedOut: true })
+        } else {
+            return reply.send({ loggedOut: false })
+        }
+    })
+
     fastify.route({
         method: "GET",
         url: "/",
-        handler: async (request, reply) => {
+        handler: async function (request, reply) {
             let home = ""
 
             if (request.session.user) {
@@ -16,164 +27,252 @@ module.exports = function (fastify, opts, done) {
         wsHandler: webSocketHandeler,
     })
 
-    async function getConvos(user_uid) {
-        const obj = await fastify.pg.transact(async (client) => {
-            const { rows } = await client.query(
-                fastify
-                    .q()
-                    .model({
-                        model: fastify
-                            .q()
-                            .model({ model: "user_conversation" })
-                            .select({ user_conversation: ["conversation_uid"] })
-                            .where({ user_conversation: { user_uid } }),
-                        as: "a",
-                    })
-                    .join({
-                        model: "user_conversation",
-                        as: "b",
-                        field: "conversation_uid",
-                        joinTable: "a",
-                    })
-                    .join({
-                        model: "users",
-                        as: "c",
-                        field: "user_uid",
-                        joinTable: "b",
-                    })
-                    .select({
-                        b: ["conversation_uid"],
-                        c: ["user_uid", "username"],
-                    })
-                    .where({
-                        b: { user_uid: `$NOT$${user_uid}` },
-                    })
-                    .eval()
-            )
-            return rows
-        })
-
-        return obj
-    }
-
-    async function getMessages(conversation_uid) {
-        const obj = await fastify.pg.transact(async (client) => {
-            const { rows } = await client.query(
-                fastify
-                    .q()
-                    .model({
-                        model: fastify
-                            .q()
-                            .model({ model: "messages" })
-                            .select()
-                            .where({ messages: { conversation_uid } }),
-                        as: "a",
-                    })
-                    .join({
-                        model: "users",
-                        as: "b",
-                        field: "user_uid",
-                        joinTable: "a",
-                    })
-                    .select({
-                        a: ["text", "message_uid", "datetime"],
-                        b: ["username", "user_uid"],
-                    })
-                    .sort({ a: ["datetime"] })
-                    .eval()
-            )
-            return rows
-        })
-
-        return obj
-    }
-
-    async function sendMessage({ user_uid, conversation_uid, text }) {
-        const obj = await fastify.pg.transact(async (client) => {
-            const { rows } = await client.query(
-                fastify
-                    .q()
-                    .model({
-                        model: "messages",
-                    })
-                    .insert({ items: [{ user_uid, conversation_uid, text }] })
-                    .returning()
-                    .eval()
-            )
-            return rows[0]
-        })
-
-        return obj
-    }
-
     const convos = {}
-
-    async function joinConvos({ socket, user_uid = "", userConvos = [] }) {
-        userConvos.forEach(({ conversation_uid }) => {
-            convos[conversation_uid] = convos[conversation_uid] || {}
-            convos[conversation_uid][user_uid] = socket
-        })
-    }
-
-    function leave(convo, user_uid) {
-        if (!convos[convo][user_uid]) return
-
-        // if the one exiting is the last one, destroy the room
-        if (Object.keys(convos[convo]).length === 1) delete convos[convo]
-        // otherwise simply leave the room
-        else delete convos[convo][user_uid]
-    }
 
     function webSocketHandeler(conn, req) {
         conn.setEncoding("utf8")
 
-        conn.socket.on("message", async (message) => {
+        conn.socket.on("message", async function (message) {
             const { type, data } = await JSON.parse(message.toString())
-            const { user_uid, username, email } = req.session?.user
+            const { user_uid, username } = req.session?.user
 
             if (type === "init") {
-                const userConvos = await getConvos(user_uid)
+                const userConvos = await dbRequest({
+                    query: getAllUserConvos({ user_uid }),
+                })
                 await joinConvos({ socket: conn.socket, user_uid, userConvos })
-
+                const userMessages = await dbRequest({
+                    query: getAllMessagesForUser({ user_uid }),
+                })
                 conn.socket.send(
                     JSON.stringify({
                         type,
                         username: `${username}`,
                         userConvos,
+                        userMessages,
                     })
                 )
             }
 
-            if (type === "getMessages") {
-                const messages = await getMessages(data)
-                conn.socket.send(JSON.stringify({ type, messages }))
-            }
-
             if (type === "sendMessage") {
                 const { conversation_uid, text } = data
-                const message = await sendMessage({
-                    user_uid,
-                    conversation_uid,
-                    text,
-                })
-
+                const message_uid = randomUUID()
                 Object.entries(convos[conversation_uid]).forEach(([, sock]) => {
                     sock.send(
                         JSON.stringify({
                             type,
-                            message: { ...message, username },
+                            message: {
+                                user_uid,
+                                conversation_uid,
+                                text,
+                                username,
+                                message_uid,
+                            },
                         })
                     )
                 })
+                await dbRequest({
+                    query: sendMessage({
+                        user_uid,
+                        conversation_uid,
+                        text,
+                        message_uid,
+                    }),
+                })
+            }
+
+            if (type === "searchUser") {
+                const users = await dbRequest({
+                    query: getUsersWithoutConvoWith({
+                        user_uid,
+                        data,
+                    }),
+                })
+
+                conn.socket.send(
+                    JSON.stringify({
+                        type,
+                        users,
+                    })
+                )
+            }
+
+            if (type === "addNewChatter") {
+                const newConvo = await dbRequest({
+                    query: createConvo({ user_uid, user_uid1: data.user_uid }),
+                })
+
+                conn.socket.send(
+                    JSON.stringify({
+                        type,
+                        newConvo,
+                    })
+                )
+
+                convos[data.user_uid]?.send(
+                    JSON.stringify({
+                        type,
+                        newConvo: [
+                            {
+                                conversation_uid: newConvo[0].conversation_uid,
+                                username: req.session?.user?.username,
+                            },
+                        ],
+                    })
+                )
+            }
+
+            if (type === "addNewConvo") {
+                convos[data] = convos[data] || {}
+                convos[data][user_uid] = conn.socket
             }
         })
+
         conn.socket.on("close", (code, reason) => {
             console.log(`${req.session.user.username} left the server`)
             Object.keys(convos).forEach((convo) =>
                 leave(convo, req.session?.user?.user_uid)
             )
+            delete convos[req.session?.user?.user_uid]
         })
     }
+
+    async function joinConvos({ socket, user_uid = "", userConvos = [] }) {
+        for (let i = 0; i < userConvos.length; i++) {
+            const { conversation_uid } = userConvos[i]
+            convos[conversation_uid] = convos[conversation_uid] || {}
+            convos[conversation_uid][user_uid] = socket
+        }
+        convos[user_uid] = socket
+    }
+
+    function leave(convo, user_uid) {
+        if (!convos[convo][user_uid]) return
+        if (Object.keys(convos[convo]).length === 1) delete convos[convo]
+        else delete convos[convo][user_uid]
+    }
+
+    async function dbRequest({ query = "" }) {
+        const client = await fastify.pg.connect()
+        let data = ""
+        try {
+            data = await client.query(query)
+        } finally {
+            client.release()
+        }
+        return data?.rows
+    }
+
+    function getAllUserConvos({ user_uid = "" }) {
+        return `
+        SELECT b.conversation_uid, c.username
+        FROM 
+        (
+            SELECT conversation_uid 
+            FROM user_conversation 
+            WHERE user_uid = '${user_uid}'
+        ) as a
+        JOIN user_conversation as b 
+        ON a.conversation_uid = b.conversation_uid
+        JOIN users as c
+        ON b.user_uid = c.user_uid
+        WHERE b.user_uid != '${user_uid}'
+        `
+    }
+
+    function getAllMessagesForUser({ user_uid = "" }) {
+        return `
+        SELECT d.message_uid, d.conversation_uid, d.user_uid, d.text, e.username 
+        FROM 
+        (
+            SELECT b.conversation_uid
+            FROM 
+            (
+                SELECT conversation_uid 
+                FROM user_conversation 
+                WHERE user_uid = '${user_uid}'
+            ) as a
+            JOIN user_conversation as b 
+            ON a.conversation_uid = b.conversation_uid
+            WHERE b.user_uid != '${user_uid}'
+        ) as c
+        JOIN messages as d
+        ON c.conversation_uid = d.conversation_uid
+        JOIN users as e
+        ON d.user_uid = e.user_uid
+        ORDER BY d.datetime;
+        `
+    }
+
+    function getUsersWithoutConvoWith({ user_uid = "", data = "" }) {
+        return `
+        SELECT user_uid, username  
+        FROM users
+        WHERE user_uid NOT IN 
+        (
+            SELECT b.user_uid
+            FROM 
+            (
+                SELECT conversation_uid 
+                FROM user_conversation 
+                WHERE user_uid = '${user_uid}'
+            ) as a
+            JOIN user_conversation as b 
+            ON a.conversation_uid = b.conversation_uid
+            WHERE b.user_uid != '${user_uid}'
+        ) AND user_uid != '${user_uid}' ${
+            data !== "" ? `AND username LIKE '%${data}%'` : ""
+        }
+        `
+    }
+
+    function sendMessage({
+        user_uid = "",
+        conversation_uid = "",
+        text = "",
+        message_uid = "",
+    }) {
+        return `
+        INSERT INTO messages 
+        (user_uid, conversation_uid, text, message_uid) 
+        values 
+        ('${user_uid}', '${conversation_uid}', '${text.replace(
+            "'",
+            "''"
+        )}', '${message_uid}')
+        RETURNING *
+        `
+    }
+
+    function createConvo({ user_uid = "", user_uid1 = "" }) {
+        return `
+        with 
+        Create_Convo (conversation_uid) as 
+            (
+                INSERT INTO conversations 
+                DEFAULT VALUES 
+                RETURNING conversation_uid
+            ),
+        First_User (conversation_uid) as 
+            (
+                INSERT INTO user_conversation (user_uid, conversation_uid) 
+                SELECT '${user_uid}', conversation_uid 
+                FROM Create_Convo 
+                RETURNING conversation_uid
+            ),
+        Second_User as (
+            INSERT INTO user_conversation (user_uid, conversation_uid) 
+            SELECT '${user_uid1}', conversation_uid
+            FROM Create_Convo 
+            RETURNING *
+        )
+        SELECT b.username, a.conversation_uid 
+        FROM Second_User as a
+        JOIN users as b
+        ON a.user_uid = b.user_uid
+        `
+    }
+
     done()
 }
 
